5c2504caaa87a65a7d714db22c09c138470e49a7
Josh Lohman <jjosh9708@gmail.com>
2017-12-12 18:03:32 -0500

    mod 5 API controller functions

--- LanguageSnippets.API/Controllers/SnippetController.cs
+++ LanguageSnippets.API/Controllers/SnippetController.cs
@@ -1,4 +1,5 @@
-﻿using LanguageSnippets.Services;
+﻿using LanguageSnippets.Models;
+using LanguageSnippets.Services;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
@@ -13,11 +14,60 @@ namespace LanguageSnippets.API.Controllers
     public class SnippetController : ApiController
     {
         //GET /api/Snippet
-        public IHttpActionResult Get()
+        public IHttpActionResult GetAll()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var snippetService = new SnippetService(userId);
+            var snippets = snippetService.GetSnippet();
+
+            return Ok(snippets);
+        }
+
+        //Get /api/Snippet/3
+        public IHttpActionResult Get(int id)
         {
             SnippetService snippetService = CreateSnippetService();
-            var snippets = snippetService.GetSnippet();
-            return Ok(snippets);
+            var snippet = snippetService.GetSnippetById(id);
+
+            return Ok(snippet);
+        }
+
+        //Put /api/Snippet
+        public IHttpActionResult Post(SnippetCreate snippet)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateSnippetService();
+
+            if (!service.CreateSnippet(snippet))
+                return InternalServerError();
+
+            return Ok();
+
+        }
+        
+        //Put /api/Snippet
+        public IHttpActionResult Put(SnippetEdit snippet)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateSnippetService();
+
+            if (!service.EditSnippet(snippet))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        //Delete /api/Snippet/3
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateSnippetService();
+
+            if (!service.DeleteSnippet(id))
+                return InternalServerError();
+
+            return Ok();
         }
 
         private SnippetService CreateSnippetService()
